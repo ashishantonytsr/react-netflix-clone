cc600ba1b2e688976afc97d376730ce89ef3d3f6
ashishashutsr <ashishantonytsr@gmail.com>
2021-07-22 19:49:59 +0530

    Some style adjustments

--- src/Components/PostsRow/PostsRow.js
+++ src/Components/PostsRow/PostsRow.js
@@ -16,16 +16,7 @@ function PostRow() {
 				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
 				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
 				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
-				<img className={classes.poster} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.xj-Ltt6j0ELPxgCfEFVQ0wHaK-%26pid%3DApi&f=1" alt="poster" />
+				
 			</div>
 		</div>
 	)
