f5f3871faa88870e58153f3aec6fc79c2b7bea11
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-01-27 20:58:29 +0530

    added .env & imported api_key from it

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
-export const baseUrl = 'https://api.themoviedb.org/3'
-export const API_KEY = '188ed00c249da8b7c38421c7e05ee34d'
-export const imgUrl = 'https://image.tmdb.org/t/p/original'
+import env from "react-dotenv";
+
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY = env.API_KEY;
+export const imgUrl = "https://image.tmdb.org/t/p/original";
