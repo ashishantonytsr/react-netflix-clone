6bdba25d31320a9a57215fe90e706bc68612db3f
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-01-27 21:03:39 +0530

    updated to in-build dotenv module

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,3 @@
-import env from "react-dotenv";
-
 export const baseUrl = "https://api.themoviedb.org/3";
-export const API_KEY = env.API_KEY;
+export const API_KEY = process.env.REACT_APP_API_KEY;
 export const imgUrl = "https://image.tmdb.org/t/p/original";
